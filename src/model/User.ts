4229520f3fae9932ab410df004d9967818ce762c
bora <a52447879@gmail.com>
2022-05-03 15:57:11 +0900

    [comment] User 스키마 주석 추가

--- src/model/User.ts
+++ src/model/User.ts
@@ -12,7 +12,11 @@ const UserSchema: Schema = new Schema({
   name: { type: String },
   email: { type: String, unquie: true },
   password: { type: String },
-  isUserVerified: { type: Boolean, default: false }, // 검증되었는가?
+  isUserVerified: { type: Boolean, default: false }, // 검증확인
 });
 
 export default model<IUser>("User", UserSchema);
+
+/**
+ * 사용자가 작성한 스키마를 기준으로 데이터를 DB에 넣기 전에 먼저 검사
+ */
